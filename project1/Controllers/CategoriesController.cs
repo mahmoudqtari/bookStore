feda6a965b03abed82a78301d4c8e2c1ab19c7a6
mahmoudqtari <mahmoudqtari5@gmail.com>
2024-05-18 00:55:29 +0300

    show category

---
+++ project1/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project1.Data;
+
+namespace project1.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public CategoriesController( ApplicationDbContext context)
+        {
+            this.context = context; 
+
+        }
+        public IActionResult Index()
+        {
+            var categories = context.categories.ToList();
+            return View(categories);
+        }
+    }
+}
--- project1/Data/ApplicationDbContext.cs
+++ project1/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using project1.Models;
 
 namespace project1.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Category> categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
